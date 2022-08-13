5a7bf465e01e1373e7c9c5cb32406cd9ffbbc99f
neggplant <cq4050@163.com>
2022-08-13 23:20:00 +0800

    添加发送rabbitmq消息（设置超时时长）接口

--- apps/baseapp/views/celery_views.py
+++ apps/baseapp/views/celery_views.py
@@ -6,7 +6,9 @@ from django.http import JsonResponse
 
 # Create your views here.
 from django.views import View
+from kombu import Connection
 
+from application.settings import BROKER_URL
 from apps.baseapp import models
 from apps.baseapp.tasks import add, sleep
 
@@ -28,3 +30,15 @@ class AsyncSleep(View):
         cache.get('my_key')
         sleep.delay(x, y)
         return JsonResponse("async_sleep:{},{}".format(x, y), safe=False)
+
+
+class RabbitMQView(View):
+
+    def get(self, request):
+        # 设置rabbitmq超时时长
+        with Connection(BROKER_URL, connection_timeout=1) as conn:
+            producer = conn.Producer(serializer='json')
+            producer.publish("111",
+                             exchange="celery", routing_key="celery",
+                             declare=["celery"])
+        return JsonResponse("RabbitMQView send", safe=False)
